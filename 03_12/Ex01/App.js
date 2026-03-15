c12bb975981c3a4b39ef608b03d26a8621568827
caiolopexd <r9506505@gmail.com>
2026-03-15 15:18:34 -0300

    .

--- 03_12/Ex01/App.js
+++ 03_12/Ex01/App.js
@@ -1,44 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const array = Array.from({ length: 10 }).map((_, index) => index);
+  const buttons = ["(",")","D","/",
+                   "7","8","9","X",
+                   "4","5","6","-",
+                   "1","2","3","+",
+                   "C","0",",","="];
 
-  const [operacao, setOperacao] = useState("");
-  const [resultado, setResultado] = useState(0);
+  const [resultado, setResultado] = useState("");
+  const [operacoes, setOperacoes] = useState("");
 
-  console.log(array);
-  array.push("+");
-  array.push("-");
-  array.push("Calcular");
 
   const handleButtonPress = (value) => {
-    console.log(`Button ${value} pressed`);
-    if (value === "Calcular") {
-      setResultado(eval(operacao));
+    if (value === "C") {
+      setResultado("");
+      setOperacoes("");
+      return;
+    }
+    if (value === "D") {
+      setOperacoes(prev => prev.slice(0, -1));
+      return;
+    }
+    if (value === "=") {
+      try{
+        setResultado(eval(operacoes));
+      }catch(error){
+        alert("Error");
+        setResultado("");
+        setOperacoes("");
+      }
       return;
     } 
     
-    setOperacao(prev => prev + value.toString());
-    setResultado(prev => prev + value.toString());
+    setOperacoes(prev => prev + value.toString());
     
   };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.head}>
+        <Text style={{fontSize: 30}}>{operacoes}</Text>
         <Text style={{fontSize: 30}}>{resultado}</Text>
       </View>
       <View style={styles.bot}>
         {
-          array.map((value) => (
-            <View key={value} style={styles.buttonWrapper}>
-              <Button title={value.toString()} onPress={() => handleButtonPress(value)} />
+          buttons.map((value,_index) => (
+            <View key={_index} style={styles.buttonWrapper}>
+
+              <Button mode="contained" buttonColor="green" onPress={() => handleButtonPress(value)} >
+                {value.toString()}
+              </Button>
             </View>
           ))
         }
-        
       </View>
     </View>
   );
@@ -47,27 +64,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16
   },
   head: {
     flex: 1,
     alignItems: "flex-end",
     justifyContent: "flex-end",
-    width: '100%',
+    width: "100%",
     padding: 20
   },
   bot: {
-    flex: 0.7,
     alignItems: "center",
     justifyContent: "center",
     flexWrap: "wrap",
     flexDirection: "row",
-    width: '100%',
+    width: "100%",
   },
   buttonWrapper: {
-    width: '33.33%',
+    width: "25%",
     padding: 5
   }
 });
